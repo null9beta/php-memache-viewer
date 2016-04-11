f8471b4e5fb66cd7e2b61d60b1cd3a7f53464377
Marco Roßdeutscher <marco@loopline-systems.com>
2016-04-11 18:58:47 +0200

    add abstract memcache command

---
+++ src/Null9beta/Memcache/Command/AbstractMemcacheCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Null9beta\Memcache\Command;
+
+use Null9beta\Memcache\Config\MemcacheConfigConstants;
+use Null9beta\Memcache\Config\MemcacheConfigFile;
+use Null9beta\Memcache\Config\MemcacheConfigInput;
+use Null9beta\Memcache\Memcache;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class AbstractMemcacheCommand extends Command
+{
+    const OPTION_CONFIG = 'config';
+    const OPTION_FILTER = 'filter';
+
+    /**
+     * setup command
+     */
+    protected function configure()
+    {
+        $this
+            ->addOption(
+                self::OPTION_CONFIG,
+                'c',
+                InputOption::VALUE_OPTIONAL,
+                'optional: path to memcache config @see config/memcache.yml.dist',
+                null
+            )
+            ->addOption(
+                MemcacheConfigConstants::SERVERS,
+                's',
+                InputOption::VALUE_OPTIONAL,
+                'optional: add server list -s localhost:11211,localhost:11212',
+                null
+            )
+            ->addOption(
+                MemcacheConfigConstants::OPTIONS,
+                'o',
+                InputOption::VALUE_OPTIONAL,
+                'optional: add options list -o OPT_LIBKETAMA_COMPATIBLE=1,OPT_...=<value>',
+                null
+            )
+            ->addOption(
+                MemcacheConfigConstants::PERSISTENT_ID,
+                'p',
+                InputOption::VALUE_OPTIONAL,
+                'optional: persist id for memcached object',
+                null
+            )
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return Memcache
+     */
+    protected function getMemcached(InputInterface $input)
+    {
+        $memcacheConfig = null;
+
+        $servers = $input->getOption(MemcacheConfigConstants::SERVERS);
+        $config = $input->getOption(self::OPTION_CONFIG);
+
+        //at least one must be set
+        if (!$servers and !$config or !($servers xor $config)) {
+            throw new \InvalidArgumentException('specify either a config file or add config via options');
+        }
+
+        if ($servers) {
+            $memcacheConfig = new MemcacheConfigInput($input);
+        }
+
+        if ($config) {
+            $memcacheConfig = new MemcacheConfigFile($config);
+        }
+
+        return new Memcache($memcacheConfig->getMemcacheInstance());
+    }
+}
--- src/Null9beta/Memcache/Command/MemcacheShowItemsCommand.php
+++ src/Null9beta/Memcache/Command/MemcacheShowItemsCommand.php
@@ -4,28 +4,22 @@ namespace Null9beta\Memcache\Command;
 
 
 use Null9beta\Memcache\Config\MemcacheConfigConstants;
-use Null9beta\Memcache\Config\MemcacheConfigFile;
-use Null9beta\Memcache\Config\MemcacheConfigInput;
-use Null9beta\Memcache\Config\MemcacheConfigInterface;
-use Null9beta\Memcache\Config\MemcacheConfigSimple;
-use Null9beta\Memcache\Memcache;
 use Null9beta\Memcache\MemcacheConfig;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class MemcacheShowItemsCommand extends Command
+class MemcacheShowItemsCommand extends AbstractMemcacheCommand
 {
-    const OPTION_CONFIG = 'config';
-    const OPTION_FILTER = 'filter';
 
     /**
      * setup command
      */
     protected function configure()
     {
+        parent::configure();
+
         $this
             ->setName('null9beta:memcache:items:show')
             ->setDescription('show items from memcache, possible filter keys')
@@ -35,34 +29,6 @@ class MemcacheShowItemsCommand extends Command
                 InputOption::VALUE_OPTIONAL,
                 'filter for keys, this is a regex string delimited with /',
                 ''
-            )
-            ->addOption(
-                self::OPTION_CONFIG,
-                'c',
-                InputOption::VALUE_OPTIONAL,
-                'optional: path to memcache config @see config/memcache.yml.dist',
-                null
-            )
-            ->addOption(
-                MemcacheConfigConstants::SERVERS,
-                's',
-                InputOption::VALUE_OPTIONAL,
-                'optional: add server list -s localhost:11211,localhost:11212',
-                null
-            )
-            ->addOption(
-                MemcacheConfigConstants::OPTIONS,
-                'o',
-                InputOption::VALUE_OPTIONAL,
-                'optional: add options list -o OPT_LIBKETAMA_COMPATIBLE=1,OPT_...=<value>',
-                null
-            )
-            ->addOption(
-                MemcacheConfigConstants::PERSISTENT_ID,
-                'p',
-                InputOption::VALUE_OPTIONAL,
-                'optional: persist id for memcached object',
-                null
             )
         ;
     }
@@ -84,32 +50,5 @@ class MemcacheShowItemsCommand extends Command
         $table->setHeaders(['Key', 'Value']);
         $table->setRows($items);
         $table->render();
-    }
-
-    /**
-     * @param InputInterface $input
-     * @return Memcache
-     */
-    private function getMemcached(InputInterface $input)
-    {
-        $memcacheConfig = null;
-
-        $servers = $input->getOption(MemcacheConfigConstants::SERVERS);
-        $config = $input->getOption(self::OPTION_CONFIG);
-
-        //at least one must be set
-        if (!$servers and !$config or !($servers xor $config)) {
-            throw new \InvalidArgumentException('specify either a config file or add config via options');
-        }
-
-        if ($servers) {
-            $memcacheConfig = new MemcacheConfigInput($input);
-        }
-
-        if ($config) {
-            $memcacheConfig = new MemcacheConfigFile($config);
-        }
-
-        return new Memcache($memcacheConfig->getMemcacheInstance());
     }
 }
